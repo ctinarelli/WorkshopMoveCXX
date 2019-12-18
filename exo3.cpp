2db7f7a7a7e31add1ab1a3e887d128e3a8ce54b1
Axel Ambert <axel.ambert@epitech.eu>
2019-12-18 11:05:05 +0100

    Add exo3

---
+++ exo3.cpp
@@ -0,0 +1,51 @@
+//
+// Created by axel on 17/12/2019.
+//
+
+#include <iostream>
+#include <array>
+
+class DummyClass
+{
+public:
+    DummyClass() : x(0), y(0)
+    {
+    }
+
+    int GetX() const
+    {
+        return (x);
+    }
+    int GetY() const
+    {
+        return (y);
+    }
+    std::array<int, 2> GetCoordinate()
+    {
+        return (std::array<int, 2>{x, y});
+    }
+private:
+    int x;
+    int y;
+};
+
+void FunctionTwo(DummyClass &&Cl)
+{
+
+}
+
+void FunctionOne(DummyClass &&Cl)
+{
+    std::cout << "je fais des trucs" << std::endl;
+    // @HERE: change something to make it compile !
+    FunctionTwo(Cl);
+}
+
+int main()
+{
+    DummyClass Dummy;
+
+    // @HERE: Basic change to add make this line compile.
+    FunctionOne(Dummy);
+    return 0;
+}
